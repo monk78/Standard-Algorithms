c1ae890360c0cda0a4dcf50c83e7d277e6ac798a
Varun Pandey <varunpandey3278@gmail.com>
2020-09-01 22:36:53 +0530

    Update distinctSubstring03.cpp

--- Trie/distinctSubstring03.cpp
+++ Trie/distinctSubstring03.cpp
@@ -1,56 +1,64 @@
-#include<iostream>
-#include<cstring>
-#include<queue>
-#include<map>
-#include<set>
-#include<stack>
-#include<cmath>
-#include<cstdio>
-#include<iomanip>
-#include<sstream>
-#include<algorithm>
- 
+//O(n^2)
+#include<bits/stdc++.h>
 using namespace std;
-#define read(x) scanf("%d",&x)
-#define Read(x,y) scanf("%d%d",&x,&y)
-#define sRead(x,y,z)  scanf("%d%d%d",&x,&y,&z)
-#define gc(x)  scanf(" %c",&x);
-#define mmt(x,y)  memset(x,y,sizeof x)
-#define write(x) printf("%d\n",x)
-#define ios   ios::sync_with_stdio(false);cin.tie(0);
-#define INF 0x3f3f3f3f
-#define ll long long
-const ll mod = 1e9 + 7;
-#define pdd pair<double,double>
-const int N = 4e5+5;
-const int  M = 1e6 + 5;
-char s[1005];
-int trie[M][26],child = 1;
-void init()
-{
-    mmt(trie,0);
-    child = 1;
+ 
+#define Fast ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+#define ld long double
+#define S second
+#define F first
+#define mp make_pair
+#define ms(x,y) memset(x,y,sizeof(x))
+#define pb push_back
+#define sl(n) cin>>n
+#define pl(n) cout<<n
+#define sze(v) (int)(v.size())
+ 
+typedef vector<int>Vl;
+typedef pair<int,int>pii;
+typedef vector<pii>Vll;
+typedef vector<vector<int>>Vv;
+typedef vector<pair<int,pii> >Vlll;
+typedef priority_queue <int, vector<int>, greater<int>>PQL;
+typedef unordered_map<int,int>ump;
+ 
+const int mod=1e9+7;
+const int modx=998244353;
+const int Nx=1e6+5;
+const int inf= (int)(1e19+10LL);
+int trie[Nx][26];
+int child=1;
+
+void solve(){
+  string x;
+  sl(x);
+  int n=sze(x);
+  int sum=0;
+  for(int i=0;i<n;i++){
+	  int parent=1;
+	  for(int j=i;j<n;j++){
+		  int ch=x[j]-'A';
+		  if(trie[parent][ch]==0){
+			  sum++;
+			  trie[parent][ch]= ++child;
+		  }
+		  parent=trie[parent][ch]; //parent for next round
+	  }
+  }
+  cout<<sum<<endl;
+  
+  
 }
-int main()
-{
-    int T;
-    read(T);
-    while(T--){
-        init();
-        scanf("%s",s);
-        int len = strlen(s);
-        int sum = 0;
-        for(int i = 0;i < len ;++i){
-            int parent = 1;
-            for(int j = i;j < len;++j){
-                int ch = s[j] - 'A';
-                if(trie[parent][ch] == 0) {
-                    sum ++;
-                    trie[parent][ch] = ++child;
-                }
-                parent = trie[parent][ch];
-            }
-        }
-        cout<<sum<<endl;
-    }
- }
+ 
+signed main(){
+   Fast;
+   int test=1;
+   sl(test);
+   while(test--){
+	   ms(trie,0);
+	   child=1;
+      solve();
+ 
+   }
+  return 0;
+}
