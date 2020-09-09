e5b1f5db68c22bd1397fd5824970616e15aa1d4d
Varun Pandey <varunpandey3278@gmail.com>
2020-09-09 12:11:03 +0530

    Update ReverseAndMore.java

--- LinkedList/ReverseAndMore.java
+++ LinkedList/ReverseAndMore.java
@@ -31,6 +31,34 @@ class methodclass{
         }
         return prev;
    }
+    static Node reverseBetween(Node headx,int m,int n){
+        if(headx==null)return null;
+        Node curr=headx,prev=null;
+        while(m>1){
+            prev=curr;
+            curr=curr.next;
+            m--;
+            n--;
+        }
+        Node con=prev,tail=curr;
+        Node third=null;
+        while(n>0){
+            third=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=third;
+            n--;
+        }
+        if(con!=null){
+            con.next=prev;
+        }
+        else {
+            headx=prev;
+        }
+        tail.next=curr;
+        return headx;
+    }
+
 };
 public class ReverseAndMore {
     public static void main(String[] args) {
@@ -44,15 +72,18 @@ public class ReverseAndMore {
         System.out.println();
         LinkedList ans=methodclass.Rev(list);
         ans.printlist();
-        Node ans2=methodclass.RevK(list.head,2);
+        Node ans2=methodclass.RevK(list.head,3);
         System.out.println();
         new LinkedList().print(ans2);
+        System.out.println();
+        Node ans3=methodclass.reverseBetween(ans2,1,3);
+        new LinkedList().print(ans3);
     }
 }
 /*
 1 2 3 4 5 
 5 4 3 2 1 
-4 5 2 3 1 
-
+3 4 5 1 2 
+5 4 3 1 2 
 
 */
