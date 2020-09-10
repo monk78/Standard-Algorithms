ada9093f7c8948957fee4917159c576d99772cf1
Varun Pandey <varunpandey3278@gmail.com>
2020-09-10 10:53:51 +0530

    Update BellmanFordMAximal.cpp

--- Graph/BellmanFordMAximal.cpp
+++ Graph/BellmanFordMAximal.cpp
@@ -1,118 +1,114 @@
-/**
-    @Author:Monk78
-    @Topic:BellmanFordMAximal
-**/
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef vector<ll>Vl;
-typedef pair<ll,ll>PII;
-typedef vector<PII>Vll;
-typedef vector<pair<ll,pair<ll,ll> > >Vlll;
-typedef priority_queue<ll>PQL;
-typedef map<ll,ll>MP;
-#define S second;
-#define F first;
-#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end));i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+ 
+#define Fast ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long
+#define ld long double
+#define S second
+#define F first
 #define mp make_pair
 #define ms(x,y) memset(x,y,sizeof(x))
 #define pb push_back
-#define mod 1000000009
-#define MAX 100005
-#define f(i,a,b) for(int i=(a);i<(b);i++)
-#define rf(i,a,b) for(int i=(a);i>=b;i--)
-#define sl(n) scanf("%lld",&n)
-#define pl(n) printf("%lld",  n)
+#define sl(n) cin>>n
+#define pl(n) cout<<n
+#define sze(v) (int)(v.size())
+ 
+typedef vector<int>Vl;
+typedef pair<int,int>pii;
+typedef vector<pii>Vll;
+typedef vector<vector<int>>Vv;
+typedef vector<pair<int,pii> >Vlll;
+typedef priority_queue <int, vector<int>, greater<int>>PQL;
+typedef unordered_map<int,int>ump;
+ 
+const int mod=1e9+7;
+const int modx=998244353;
+const int Nx=1e5+2;
+const int inf= (int)(1e19+10LL);
+
 struct edge{
-    ll a,b,cst;
+    int a,b,cst;
 };
-ll vert,edg,src;
-const ll INF = 1000000000;
+int vert,edg,src;
 vector<edge>graph;
-void solve(){
-    Vl dist(vert+5,INF);
+
+void Bellman(){
+    Vl dist(vert+5,inf);
     Vl parent(vert+5,-1);
     dist[src]=0;
-    ll negative=-1;
-    for(ll i=0;i<vert;i++){
+    int negative=-1;
+    for(int i=0;i<vert;i++){
          negative=-1;
-        for(ll j=0;j<edg;j++){
-                ll from=graph[j].a;
-                ll to=graph[j].b;
-                ll wt=graph[j].cst;
-                if(dist[from]<INF)
-                {
-                    if(dist[to]>dist[from]+wt){
-                    dist[to]=min(dist[to],dist[from]+wt);
-                    parent[to]=from;
-                    negative=to;
-                    }
-
-
-                }
+        for(int j=0;j<edg;j++){
+                int from=graph[j].a;
+                int to=graph[j].b;
+                int wt=graph[j].cst;
+                if(dist[to]>dist[from]+wt){
+				   dist[to]=max(-inf,dist[from]+wt);
+				   parent[to]=from;
+				   negative=to;	
+				}
         }
     }
     if(negative==-1)cout<<"No negative cycle:"<<endl;
     else{
-        ll y=negative;
-        for(ll i=1;i<=vert;i++){
+		cout<<"There is a negative cycle"<<endl;
+        int y=negative;
+        for(int i=1;i<=vert;i++){
             y=parent[y];
         }
         Vl path;
-        for(ll curr=y; ;curr=parent[curr]){
+        for(int curr=y; ;curr=parent[curr]){
             path.pb(curr);
             if(curr==y && path.size()>1)break;
         }
         reverse(path.begin(),path.end());
-        cout<<"Negative cycle:"<<endl;
         for(auto it:path)cout<<it<<"";
+        cout<<endl;
+        return;
     }
-
-    /*
-    for(ll i=1;i<=vert;i++){
+   cout<<"distances to reach destinations:"<<endl;
+    for(int i=1;i<=vert;i++){
         pl(dist[i]);
         cout<<" ";
-    }*/
-    /*
-    ll t=4;
-    if(dist[t]==INF)cout<<"No path";
+    }
+    int t=4;
+    if(dist[t]==inf)cout<<"No path";
     else{
+		cout<<endl<<"path to rech destination:"<<t<<endl;
         Vl path;
-        for(ll curr=t;curr!=-1;curr=parent[curr]){
+        for(int curr=t;curr!=-1;curr=parent[curr]){
             path.pb(curr);
         }
         reverse(path.begin(),path.end());
-        for(auto it:path)cout<<it<<" ";
-
+        for(auto it:path)cout<<it<<"->";
     }
-    */
-
-
-
+    cout<<endl;
 }
-signed main(){
-    ll u,v,wt;
+void solve(){
+	int u,v,wt;
     sl(vert);
     sl(edg);
-    for(ll i=0;i<edg;i++){
+    for(int i=0;i<edg;i++){
         sl(u);sl(v);sl(wt);
-        edge E1,E2;
-        //E2.a=v;
-        //E2.b=u;
-        //E2.cst=wt;
+        edge E1;
         E1.a=u;
         E1.b=v;
         E1.cst=wt;
         graph.pb(E1);
-        //graph.pb(E2);
     }
     src=1;
-    solve();
-    /*
-    for(ll i=0;i<edg;i++){
-        cout<<graph[i].a<<" "<<graph[i].b<<" "<<graph[i].cst<<endl;
-    }*/
+    Bellman();
+}
 
 
+signed main(){
+   int test;
+   sl(test);
+   while(test--){
+	   graph.clear();
+	   solve();
+   }
+   
 return 0;
 }
