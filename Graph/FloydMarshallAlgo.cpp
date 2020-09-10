604ae9197f311d7395a323c76a25c272bc138988
Varun Pandey <varunpandey3278@gmail.com>
2020-09-10 11:29:10 +0530

    Update FloydMarshallAlgo.cpp

--- Graph/FloydMarshallAlgo.cpp
+++ Graph/FloydMarshallAlgo.cpp
@@ -1,13 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
+ 
 #define Fast ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define int long long
-typedef vector<int>Vl;
-typedef pair<int,int>PII;
-typedef vector<PII>Vll;
-typedef vector<pair<int,pair<int,int> > >Vlll;
-typedef priority_queue<int>PQL;
-typedef map<int,int>M;
+#define ld long double
 #define S second
 #define F first
 #define mp make_pair
@@ -15,13 +11,26 @@ typedef map<int,int>M;
 #define pb push_back
 #define sl(n) cin>>n
 #define pl(n) cout<<n
+#define sze(v) (int)(v.size())
+ 
+typedef vector<int>Vl;
+typedef pair<int,int>pii;
+typedef vector<pii>Vll;
+typedef vector<vector<int>>Vv;
+typedef vector<pair<int,pii> >Vlll;
+typedef priority_queue <int, vector<int>, greater<int>>PQL;
+typedef unordered_map<int,int>ump;
+ 
 const int mod=1e9+7;
-const int sze=1e5+2;
-const int inf= 0x3f3f3f3f;
- int p[1000+1][1000+1];
+const int modx=998244353;
+const int Nx=1e5+2;
+const int inf= (int)(1e19+10LL);
+
+int parent[1000+1][1000+1];
+
 void print_path (int i, int j) {
   if (i!=j)
-    print_path(i,p[i][j]);
+    print_path(i,parent[i][j]);
   cout<<j+1<<" ";
 }
 void solve(){
@@ -33,8 +42,8 @@ void solve(){
 	 for(int j=0;j<n;j++){
 		 if(i==j)d[i][j]=0;
 		 else
-		 d[i][j]=mod;
-	   p[i][j]=i; 
+		 d[i][j]=modx;
+	   parent[i][j]=i; 
 	 }
  }
  
@@ -51,8 +60,16 @@ void solve(){
     for (int j=0;j<n;j++)
       if (d[i][k] + d[k][j] < d[i][j]) {
         d[i][j] = d[i][k]+d[k][j];
-        p[i][j] = p[k][j];
+        parent[i][j] = parent[k][j];
       }
+      
+   for(int i=0;i<n;i++){
+	   for(int j=0;j<n;j++){
+           cout<<d[i][j]<<" ";		   
+	   }
+	   cout<<endl;
+   }   
+   cout<<endl;
    print_path(0,n-1);     
       
  }
